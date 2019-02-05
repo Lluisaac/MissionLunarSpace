a81fdeecdf85dca43bb505ec79e7ea249e6fdd06
Azuri902 <46462194+Azuri902@users.noreply.github.com>
2019-02-05 20:46:31 +0100

    Num 2 (sans bug)

--- app/src/main/java/com/acpi/mls/missionlunarspace/EtudiantActivity.java
+++ app/src/main/java/com/acpi/mls/missionlunarspace/EtudiantActivity.java
@@ -1,10 +1,11 @@
 package com.acpi.mls.missionlunarspace;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class EtudiantActivity extends AppCompatActivity {
 
@@ -19,17 +20,24 @@ public class EtudiantActivity extends AppCompatActivity {
     }
 
     public void onClikButtonContinuer(View view) throws InterruptedException {
-       EditText etuNom  = findViewById (R.id.textViewNomEtu);
-       this.nomEtu = etuNom.getText().toString();
+        EditText etuNom  = findViewById (R.id.textViewNomEtu);
+        String nom = etuNom.getText().toString();
 
-       EditText etuClasse = findViewById(R.id.textViewClasseEtu) ;
-       this.classeEtu = etuClasse.getText().toString();
-       setContentView(R.layout.content_etudiant_attente);
+        EditText etuClasse = findViewById(R.id.textViewClasseEtu) ;
+        String classe = etuClasse.getText().toString();
+
+        if(verifText(nom,classe)) {
+            this.nomEtu = nom;
+            this.classeEtu = classe;
+            setContentView(R.layout.content_etudiant_attente);
+        }else {
+            Toast.makeText(this, "Vous devez renseigner tous les champs !", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onCLickTempo(View view)
     {
-        Intent intent = new Intent(this,ChoixPerso_activity.class);
+        Intent intent = new Intent(this,ChoixPersoActivity.class);
         Bundle bundle = new Bundle();
         bundle.putString("NomEtu", nomEtu);
         bundle.putString("ClasseEtu",classeEtu);
@@ -38,4 +46,7 @@ public class EtudiantActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private boolean verifText(String nom, String classe) {
+        return !classe.equals("") && !nom.equals("");
+    }
 }
