b8c0836df1dfea182d4eb1db5e4a586723e69680
Azuri902 <46462194+Azuri902@users.noreply.github.com>
2019-02-06 10:33:27 +0100

    DebutChoixPersoEtu

--- app/src/main/java/com/acpi/mls/missionlunarspace/ChoixPersoActivity.java
+++ app/src/main/java/com/acpi/mls/missionlunarspace/ChoixPersoActivity.java
@@ -2,12 +2,24 @@ package com.acpi.mls.missionlunarspace;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ChoixPersoActivity extends AppCompatActivity {
+
+    private String nom;
+    private String classe;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choix_perso);
+        this.nom = (String) getIntent().getSerializableExtra("NomEtu");
+        this.classe = (String) getIntent().getSerializableExtra("ClasseEtu");
+    }
+
+    private void creationList() {
     }
 }
