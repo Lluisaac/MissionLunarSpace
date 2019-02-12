68646547aedef3f4b43d4b2e9449d53ea584d6ef
Azuri902 <46462194+Azuri902@users.noreply.github.com>
2019-02-12 11:38:20 +0100

    Revert "revert apres demo"
    This reverts commit 98712ea516f6c73dec9353f6389f5c1eaf6d6aae.

--- app/src/main/java/com/acpi/mls/missionlunarspace/EtudiantActivity.java
+++ app/src/main/java/com/acpi/mls/missionlunarspace/EtudiantActivity.java
@@ -85,7 +85,7 @@ public class EtudiantActivity extends AppCompatActivity {
         setContentView(R.layout.content_etudiant_attente);
         Button boutonSuiveAttente = (Button) findViewById(R.id.buttonTempoAttente);
         //TODO changer par boutonSuiveAttente.setVisibility(View.INVISIBLE);
-        boutonSuiveAttente.setVisibility(View.INVISIBLE);
+        boutonSuiveAttente.setVisibility(View.VISIBLE);
         new DAOCheckPartieDemaree(boutonSuiveAttente, Integer.parseInt(idClasse)).execute();
     }
 
